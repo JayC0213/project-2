e055bf3a7bbde7e06768c3daa26b53798abceeac
JayC0213 <juliancabadas@yahoo.com>
2025-03-07 22:58:41 -0800

    update

---
+++ js/main.js
@@ -0,0 +1,65 @@
+
+// Setting Volume at a certain point so no one's speakers or headphones blow up. Feel free to adjust how you want though with the controls :D
+
+backgroundAudio=document.getElementById("myCarMusic");
+backgroundAudio.volume=0.25;
+
+// First Car Section
+
+document.querySelector("#car-1").addEventListener("click", function(){
+    document.querySelector("#other-car1-1").style.visibility = "visible";
+    console.log("testing");
+})
+
+document.querySelector("#other-car1-1").addEventListener("click", function(){
+    document.querySelector("#other-car1-2").style.visibility = "visible";
+    console.log("testing");
+})
+
+// Second Car Section
+
+document.querySelector("#car-2").addEventListener("click", function(){
+    document.querySelector("#other-car2-1").style.visibility = "visible";
+    console.log("testing");
+})
+
+document.querySelector("#other-car2-1").addEventListener("click", function(){
+    document.querySelector("#other-car2-2").style.visibility = "visible";
+    console.log("testing");
+})
+
+// Third Car Section
+
+document.querySelector("#car-3").addEventListener("click", function(){
+    document.querySelector("#other-car3-1").style.visibility = "visible";
+    console.log("testing");
+})
+
+document.querySelector("#other-car3-1").addEventListener("click", function(){
+    document.querySelector("#other-car3-2").style.visibility = "visible";
+    console.log("testing");
+})
+
+// Fourth Car Section
+
+document.querySelector("#car-4").addEventListener("click", function(){
+    document.querySelector("#other-car4-1").style.visibility = "visible";
+    console.log("testing");
+})
+
+document.querySelector("#other-car4-1").addEventListener("click", function(){
+    document.querySelector("#other-car4-2").style.visibility = "visible";
+    console.log("testing");
+})
+
+// Fifth Car Section
+
+document.querySelector("#car-5").addEventListener("click", function(){
+    document.querySelector("#other-car5-1").style.visibility = "visible";
+    console.log("testing");
+})
+
+document.querySelector("#other-car5-1").addEventListener("click", function(){
+    document.querySelector("#other-car5-2").style.visibility = "visible";
+    console.log("testing");
+})
